1b422734f516f0dee3fa6a5d8fed1c456bd112d6
Ricardo_perez <infoarte247@gmail.com>
2021-08-19 14:24:10 -0700

    map no funciona coneccion firebase ok

--- pages/datafire/index.js
+++ pages/datafire/index.js
@@ -1,30 +1,58 @@
  
 import firebase from 'firebase/app'; 
 import 'firebase/firestore';
-// import { useCollection } from "react-firebase-hooks/firestore";
+ import { useCollection } from "react-firebase-hooks/firestore";
 
-
-
-
-let Home = ()=> {
-
-
-   firebase.firestore().collection('blog-cogigo').add({
-    data:'cocola'
-  }).then(data=>data.json()).then(datas=>console.log(datas))  
-   
-     
-
+ 
+    
+let Home = ({mydata})=> {
+ 
 return (
-    <>
-    </>
+    <div>
+    { 
+    mydata.map(data=>(
+      <p>{data[0].nameLiker}</p>
+    ))
+    }
+    </div>
 )
     
+  }
+  
+
+export async function getStaticProps() {  
+    
+
+    let allData = [];
+
+  let data = await firebase.firestore().collection('sistemLikes').get(); 
+     data.docs.forEach(da=>{
+       
+       allData.push(da.data())
+    })
+   
 
 
 
-    }
+  // let cocu =  data.data();
+  
+    //  console.log(cocu)
+      
+      /*
+      esto se renderiza del lado del servidor por lo que 
+      el ersultado se vera en la pantalla en donde se eje
+      cuta {npm dev} y no en la consola del navegador
+      */
+  return {
+    props: {
+      mydata: JSON.stringify(allData),
+    },
+  }
+}
 
+
+
+  
 
 
   export default Home
