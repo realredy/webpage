c69c9303356c52b3d94bdbbfcbf0c5bda4ae026a
Ricardo_perez <infoarte247@gmail.com>
2021-09-07 15:21:49 -0700

    llamado de data categories e imagenes coordinados a la perfeccion

--- components/articles/setnewarticle.js
+++ components/articles/setnewarticle.js
@@ -1,14 +1,19 @@
- import React, { useRef } from 'react';
+ import React, { useRef, useState } from 'react';
  import dynamic from "next/dynamic";
  import 'suneditor/dist/css/suneditor.min.css'; 
  import firebase from 'firebase'; 
 import {db} from '../../firebase/firebase';
 import 'firebase/firestore';
+import Panelmultimedia from '../multimedia/panelmultimedia';
 
  const SunEditor = dynamic(() => import("suneditor-react"), {
     ssr: false,
   });
- function SetNewArticle({categories}){
+
+   
+ function SetNewArticle({categories, conoo}){
+     const [pannelMM, setpannelMM] = useState()
+
      const categoriesToJson = JSON.parse(categories)
      const categoiesToArray = Object.values(categoriesToJson) 
         
@@ -16,10 +21,17 @@ import 'firebase/firestore';
    function handleChange(content){
         console.log('Sun editor::::;',content); //Get Content Inside Editor
     }
+
+    let selectIMGforArticle = (e)=>{
+        e.preventDefault()
+        setpannelMM(<Panelmultimedia allImages={conoo} />)
+          
+  }
  
   // documentacion sun editor::https://github.com/mkhstar/suneditor-react
     return(
-        <>
+        <> 
+        {pannelMM}
         <div className="SetNewArticle">
             <div className="SetNewArticle__wrapper-title">
              <span>Create new article</span>
@@ -58,7 +70,7 @@ import 'firebase/firestore';
                                 <div className="SetNewArticle__body-wrapper-form-aside-selectIMG-preview">
 
                                 </div> 
-                                <button id="selector-img-artc">Select image article</button>
+                                <button onClick={selectIMGforArticle} id="selector-img-artc">Select image article</button>
                             </div>
                             <div className="SetNewArticle__body-wrapper-form-aside-selectCategory">
                                 <span>Select Category</span>
---
+++ components/multimedia/multimedia.js
@@ -0,0 +1,108 @@
+import firebase from 'firebase/app'; 
+import {db} from '../../firebase/firebase';
+import 'firebase/storage'; 
+import React,{useEffect, useState} from 'react';
+
+//====================================================//
+// ---------------🕯 pre visualiuzador 🕯--------------- //
+//====================================================// 
+var preloader;
+let preview_image = (e) =>{ 
+    console.log(e)
+const image = e.target.files[0];
+const IMG_reader = new FileReader(); 
+IMG_reader.onload = function(){
+const metadata_img = IMG_reader.result;
+const output = document.getElementsByClassName('multimedia__wrapper--right-wrapper-preloader')[0];
+preloader = output
+output.src =  metadata_img;  
+};
+IMG_reader.readAsDataURL(image);
+return e; 
+}
+//====================================================//
+// ---------------🕯 pre visualiuzador 🕯--------------- //
+//====================================================// 
+
+
+
+//====================================================//
+// ---------- - UPLOAD 🦁to⬆ FIREBASE --------------- //
+//====================================================// 
+let save_image = (e) => {  
+    const form = document.getElementsByClassName('multimedia__wrapper--left-form')[0]
+    const loading_bar = document.getElementsByClassName('multimedia__wrapper--left-preloader-bar')[0]
+    const uploadBTN = document.getElementById('uploadBTN') 
+    e.preventDefault();
+    const image = e.target[1].files[0];
+    const image_type = e.target[1].files[0].type;
+    const image_name = e.target[1].files[0].name;
+    const title = e.target[0].value;  
+    let extDividida = image_type.split('/')[1]; 
+    const dbs = firebase.storage(db);
+    const storag =  dbs.ref('all/'+ image_name+'.'+extDividida);
+    const metadata = {
+        customMetadata: {
+          'alt': title
+        }
+      }
+      
+    const subio = storag.put(image, metadata);
+ subio.on('state_changed', function(snapshot){ 
+        // ==== console.log(snapshot) === //
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        uploadBTN.disabled = true; 
+        uploadBTN.value = 'Uploading..'
+        loading_bar.style.width = progress + '%';
+        console.log(progress) 
+        }, function(error){
+            console.log("Error al subir imagen",error);
+        }, function(){
+        dbs.ref('all/').child(image_name+'.'+extDividida).getDownloadURL()
+        .then( ur=>{ 
+            uploadBTN.disabled = false; 
+        uploadBTN.value = 'upload image'
+        loading_bar.style.width = 0;
+        preloader.src = '';
+        form.reset()
+        console.log(ur) 
+        })
+    })
+}
+//====================================================//
+// ---------------- UPLOAD to FIREBASE --------------- //
+//====================================================// 
+
+let Multimedia = () => {
+
+    return (
+    <>
+    <div className="multimedia">
+        <div className="multimedia__wrapper">
+            <div className="multimedia__wrapper--left">
+                <span>Upload multimedia</span>
+                <form onSubmit={save_image} className="multimedia__wrapper--left-form">
+                    <label htmlFor="mult_alt">Write alt to image</label>
+                    <input type="text" name="alt" id="mult_alt" />
+                    <label htmlFor="image_upload">Select Image 800 X 325</label>
+                    <input onChange={preview_image} type="file" name="image" id="image_upload" />
+                    <input id="uploadBTN" type="submit" value="upload image" />
+                </form>
+                <div className="multimedia__wrapper--left-preloader">
+                    <div className="multimedia__wrapper--left-preloader-bar"></div>
+                </div>
+            </div>
+            <div className="multimedia__wrapper--right">
+                <div className="multimedia__wrapper--right-wrapper">
+                    <img src=""  className="multimedia__wrapper--right-wrapper-preloader" />
+
+                     
+                    <span>Image preview</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+   )
+}
+export default Multimedia
